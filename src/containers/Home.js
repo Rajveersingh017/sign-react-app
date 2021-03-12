93037e55a9745334bbf64aeff73f9fc141ee06f9
Rajveer <rsingh91@Aceproject.space>
2021-03-12 10:03:46 -0600

    forgot to save the home.js file in last commit

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -9,76 +9,6 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 export default function Home() {
   return (
-<<<<<<< HEAD
-    // <div className="Home">
-    //   <div className="lander">
-    //   <Card style={{ width: '36rem' }} className="flex" > 
-    //     <Card.Img variant="top" src="https://www.canadahelps.org/media/charity_clipart.jpg" className="img-fluid" alt="" />
-    //     <Card.Body>
-    //       <Card.Title>Card Title</Card.Title>
-    //       <Card.Text>
-    //         Some quick example text to build on the card title and make up the bulk of
-    //         the card's content.
-    //       </Card.Text>
-    //     </Card.Body>
-    //     <Card.Body>
-    //       <Card.Link href="#">Card Link</Card.Link>
-    //       <Card.Link href="#">Another Link</Card.Link>
-    //     </Card.Body>
-    //   </Card>
-    //   <br />
-    //   <Card style={{ width: '36rem' }}>
-    //     <Card.Img variant="top" src="https://mdbootstrap.com/img/Others/documentation/1.jpg" className="img-fluid" alt="" />
-    //     <Card.Body>
-    //       <Card.Title>Card Title</Card.Title>
-    //       <Card.Text>
-    //         Some quick example text to build on the card title and make up the bulk of
-    //         the card's content.
-    //       </Card.Text>
-    //     </Card.Body>
-    //     <Card.Body>
-    //       <Card.Link href="#">Card Link</Card.Link>
-    //       <Card.Link href="#">Another Link</Card.Link>
-    //     </Card.Body>
-    //   </Card>
-    //   </div>
-    //   <div>
-    //     <Card className="text-center">
-    //       <Card.Header>Please Consider Making a Difference and Donating</Card.Header>
-    //       <Card.Body>
-    //         <Card.Title>Donate</Card.Title>
-    //         <Card.Text>
-    //           With your support, together we can make a difference to those less fortunate
-    //         </Card.Text>
-    //         <Button variant="primary" href="https://www.facebook.com/TheSIGNFoundation" target="_blank">Donate through Facebook</Button>{' '}
-            
-    //         <Button variant="secondary" href="https://www.thesignfoundation.org/" target="_blank" >Donate through our Website</Button>
-    //       </Card.Body>
-    //       <Card.Footer className="text-muted">Some text can go here</Card.Footer>
-    //     </Card>
-    //   </div>
-    // </div>
-    <div>
-      <Container>
-      <Row>
-        <Carousel>
-          <Carousel.Item>
-            <Col sm class="cardAgency">
-            <Image src="https://www.canadahelps.org/media/charity_clipart.jpg" fluid />
-            sm=true
-            </Col></Carousel.Item>
-            <Carousel.Item><Col sm class="cardAgency">sm=true
-            <Image src="https://www.canadahelps.org/media/charity_clipart.jpg" fluid />
-            </Col></Carousel.Item>
-            <Carousel.Item><Col sm class="cardAgency">sm=true
-            <Image src="https://www.canadahelps.org/media/charity_clipart.jpg" fluid />
-            </Col></Carousel.Item>
-          
-        </Carousel>
-      </Row>
-    </Container>
-    </div> 
-=======
     <div className="Home">
       <div className="lander">
       <Card style={{ width: '36rem' }} className="flex" > 
@@ -127,7 +57,6 @@ export default function Home() {
         </Card>
       </div>
     </div>
->>>>>>> 2f8486dd500d732a3dc39a7717f2e2f026ef9ce7
   );
 }
 
