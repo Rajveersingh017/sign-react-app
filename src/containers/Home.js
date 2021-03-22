052d2f784a031fb585703f547b9ebbf32117b694
Haris Saran <hsaran100@academic.rrc.ca>
2021-03-22 10:33:42 -0500

    small change to Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,6 +7,7 @@ import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+
 export default function Home() {
   return (
     <div className="Home">
